954f3abf2159be4a2708a69836e4d791fbf4eec5
Slayden Z Gruneberg <122947683+s-gruneberg@users.noreply.github.com>
2025-05-19 00:02:25 -0700

    changes to faq

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -37,12 +37,15 @@ export default function About() {
                     className={darkMode ? 'bg-dark text-light border-secondary' : ''}
                 >
                     <Accordion.Header>
-                        Does this site track me or take my data?
+                        Is this site affiliated with Alcoholics Anonymous?
                     </Accordion.Header>
                     <Accordion.Body className={darkMode ? 'bg-dark text-light' : ''}>
-                        Short answer: No. Some IP address data is collected by the cloud
-                        hosting provider, which is standard, but all journal entry data is
-                        only ever stored on your browser and never uploaded anywhere.
+                        No. This site is not affiliated with Alcoholics Anonymous or any other 12-step program. 
+                        It is a personal project created to help people in recovery.
+                        <br />
+                        <br />
+                        I (the creator) am a member of Alcoholics Anonymous and wanted to create a tool that 
+                        could help others in recovery.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item
@@ -53,7 +56,7 @@ export default function About() {
                         Is this site free?
                     </Accordion.Header>
                     <Accordion.Body className={darkMode ? 'bg-dark text-light' : ''}>
-                        Yes. This site is free to use and will always be. It does not require any payment or subscription.
+                        Yes. This site is free to use and always will be.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item
@@ -61,10 +64,28 @@ export default function About() {
                     className={darkMode ? 'bg-dark text-light border-secondary' : ''}
                 >
                     <Accordion.Header>
+                        Is my data safe?
+                    </Accordion.Header>
+                    <Accordion.Body className={darkMode ? 'bg-dark text-light' : ''}>
+                        If you don't have an account, no data ever leaves your device.
+                        <br />
+                        <br />
+                        If you have an account, your custom questions and information about your streaks
+                        are stored in an industry standard secure way.
+                        </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item
+                    eventKey="7"
+                    className={darkMode ? 'bg-dark text-light border-secondary' : ''}
+                >
+                    <Accordion.Header>
                         Do I have to create an account to use this site?
                     </Accordion.Header>
                     <Accordion.Body className={darkMode ? 'bg-dark text-light' : ''}>
-                        No. You can use this site without creating an account. However, if you create an account, you can
+                        No. You can use this site without creating an account. 
+                        <br />
+                        <br />
+                        However, if you create an account, you can
                         track your daily activity streaks and your total days in recovery on the Insights page.
                     </Accordion.Body>
                 </Accordion.Item>
@@ -73,24 +94,19 @@ export default function About() {
                     className={darkMode ? 'bg-dark text-light border-secondary' : ''}
                 >
                     <Accordion.Header>
-                        What data is stored on the cloud if I have an account?
+                        Are my answers to the questions kept private?
                     </Accordion.Header>
                     <Accordion.Body className={darkMode ? 'bg-dark text-light' : ''}>
-                        If you have an account, this data is stored on the cloud to allow you to access your account from any device.
-                        It is stored in an industry standard way that is commonly used and secure.
-                        <ul>
-                            <li>Your custom questions</li>
-                            <li>Your custom checkmarks</li>
-                            <li>Your journal streaks</li>
-                            <li>Your total days in recovery</li>
-                            <li>Your recovery date</li>
-                            <li>Your username</li>
-                            <li>Your email</li>
-                            <li>Your encrypted password</li>
-                            <li>Your account creation date</li>
-                            <li>Your last login date</li>
-                        </ul>
-
+                        Yes.
+                        <br />
+                        <br />
+                        Your answers are temporarily stored in your browser's session storage, 
+                        which means that they will be lost if you close your broswer tab or 
+                        clear browsing data. 
+                        <br />
+                        <br />
+                        The answers your write to the questions never leave your device. 
+                        This is true even if you have an account and are logged in.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item
@@ -101,25 +117,13 @@ export default function About() {
                         How do my custom questions get remembered even if I don't have an account?
                     </Accordion.Header>
                     <Accordion.Body className={darkMode ? 'bg-dark text-light' : ''}>
-                        In an effor to provide user anonymity, a feature was added to make an effort
-                        at remembering questions from anonymous users. The way this works is that
-                        the questions are stored on your device's browser in something called localStorage. Purging
-                        your browser history, browser cache etc will likely delete this information. It's stored locally on
+                        The way this works is that the questions are stored on your device's browser in something called localStorage. 
+                        <br />
+                        Purging your browser history, browser cache etc will likely delete this information. It's stored locally on
                         your browser and is not uploaded anywhere.
                     </Accordion.Body>
                 </Accordion.Item>
-                <Accordion.Item
-                    eventKey="5"
-                    className={darkMode ? 'bg-dark text-light border-secondary' : ''}
-                >
-                    <Accordion.Header>
-                        Can I upload my journal entries if I have an account?
-                    </Accordion.Header>
-                    <Accordion.Body className={darkMode ? 'bg-dark text-light' : ''}>
-                        For now, no. This is a feature that is being considered for the future.
-                        You can download your journal entries as a PDF, raw text, or word document.
-                    </Accordion.Body>
-                </Accordion.Item>
+               
                 <Accordion.Item
                     eventKey="6"
                     className={darkMode ? 'bg-dark text-light border-secondary' : ''}
@@ -128,8 +132,16 @@ export default function About() {
                         Why not make an app?
                     </Accordion.Header>
                     <Accordion.Body className={darkMode ? 'bg-dark text-light' : ''}>
-                        Making a web app rather than a native app was a conscious decision, if you want to
-                        use this site like a native app, you can do so by adding it to your home screen.
+                        If you want to use this site like a native app, you can do so by adding it to your home screen!
+                        <br />
+                        <br />
+                        On an iPhone, you can do this by going to the share menu in Safari and selecting "Add to Home Screen".
+                        <br />
+                        <br />
+                        On Android, you can do this by going to the menu in Chrome and selecting "Add to Home Screen".
+                        <br />
+                        <br />
+                        This will create an icon on your home screen that will open the site in a standalone window.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
