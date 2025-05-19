e0391dc5c48314aa5b3c0e1e060303f4a77ea116
Slayden Z Gruneberg <122947683+s-gruneberg@users.noreply.github.com>
2025-05-18 17:38:19 -0700

    remove todo testing deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -41,16 +41,4 @@ function App() {
 
 export default App
 
-/* TODO
-- Make header text fit 
-- Add pages: nightly inventory, home(?), meditation and prayer / upon awakening (?), customize questions
-- Consider moving light/dark button or changing color (not sure how to change color)
-- Save to pdf functionality
-- Custom question functionality
-- Making sure it can extend out to a backend that stores user's custom questions, morning meditation/prayer streaks with data, 
-- Home page explaining step 10 and what's going on with a link to nightly inventory, maybe a selective navigation: if user has visited before, go to nightly inventory page
-- making sure local storage data is structured correctly, ie key: 10stepjournal: dark:true, questions: ["question 1", "question2"...]
-- if you open mobile menu, then click something else, mobile menu closes
-- persist the answers to local storage but only during the session 
 
-*/
