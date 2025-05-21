5a6f39a2ba3bc8c686f32e4e13e4e66bfeca8c19
Slayden Z Gruneberg <122947683+s-gruneberg@users.noreply.github.com>
2025-05-21 06:41:42 -0700

    Changed home frontend

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -11,16 +11,16 @@ const Home = () => {
 
   return (
     <div className="container mt-4">
-      <h1 className="mb-2">Welcome to 10th Step Journal</h1>
+      <h1 className="h1 mb-2">Welcome to 10th Step Journal!</h1>
       <hr className="mb-5 mt-0" />
 
-      <p className="lead">
+      <p className="h3 mb-3">
         This site helps you complete your daily 10th Step inventory.
       </p>
-
-      <p className="lead">
-        No account required, no data collection, and no ads.
-      </p>
+      <ul className="lead">
+        <li>You can customize the inventory questions.</li>
+        <li>Accounts are not required, there is no data collection, and there are no ads.</li>
+      </ul>
 
       <div className="mt-4">
         <Link to="/inventory" className={primaryButtonClass}>
