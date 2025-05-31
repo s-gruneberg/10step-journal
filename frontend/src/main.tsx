38873a4cd489215b8d70c0b1a4ecfbe9e7ea8c52
Slayden Z Gruneberg <122947683+s-gruneberg@users.noreply.github.com>
2025-05-31 11:50:39 -0700

    added service worker

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
 import { DarkModeProvider } from './context/DarkModeContext'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
@@ -8,6 +8,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import './index.css'
 import { ensureQuestionsInitialized } from './localStorageUtils'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 
 ensureQuestionsInitialized();
@@ -19,3 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </DarkModeProvider>
   </React.StrictMode>
 )
+
+// Register service worker
+serviceWorkerRegistration.register();
