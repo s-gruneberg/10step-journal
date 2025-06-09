3643884ba7b7198619b77abee7cecb2c68b1cafa
Slayden Z Gruneberg <122947683+s-gruneberg@users.noreply.github.com>
2025-06-09 09:37:09 -0700

    updated jwt settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -195,11 +195,12 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
 }
 
-
-# Security Settings
-# if not DEBUG:
-#     SECURE_SSL_REDIRECT = True
- 
 
